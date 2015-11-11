f0f2436bc01c8d6409f8459c8e2adce364168c60
Yair Smith <yairyach@hotmail.com>
2015-11-11 16:49:55 +0200

    HTTP Stuff

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -7,11 +7,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net.Http;
 
 namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
     {
+        private const string API_KEY = "AIzaSyAWNH7Ny4GFvJ_485ObBWE7RrJ-K9cuTqQ";
+
+        private string examplePlaylist = "https://www.youtube.com/playlist?list=PL1oVHjBfBpwCuKJ0dynZyslyApZPwz9jr";
+
         public Form1()
         {
             InitializeComponent();
@@ -19,7 +24,21 @@ namespace WindowsFormsApplication1
 
         private void button1_Click(object sender, EventArgs e)
         {
+            var playlistId = "PL1oVHjBfBpwCuKJ0dynZyslyApZPwz9jr";
 
+            HttpClient httpClient = new HttpClient();
+            string getRequestURL = string.Format("https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId={0}&key={1}", playlistId, API_KEY);
+            string response = httpClient.GetStringAsync(getRequestURL).ToString();
+
+            System.Diagnostics.Debug.Print(response.ToString());
+
+        }
+
+        private string getPlaylistId(string playlistUrl)
+        {
+            if (playlistUrl.Contains("?list="))
+                return playlistUrl.Split(new string[] { "?list=" }, StringSplitOptions.None)[1];
+            else return "Error: Invalid playlist URL.";
         }
     }
 }
